571a2f3dfa03cdbf08c439a37c12c4a37f0368d8
TomeT187 <tomet187@gmail.com>
2024-05-30 19:36:34 -0700

    scene selection started

--- DND_UI.py
+++ DND_UI.py
@@ -1,15 +1,48 @@
 import tkinter as tk
+from os import listdir
 
 class DNDWindow(tk.Tk):
+    
     def __init__(self):
         super().__init__()
-        self.geometry("400x400")
+        
+        self.geometry("1920x1080")
+        self.backGroundPic = tk.PhotoImage(file = "images\\welcome.png")
+        self.scenes = []
         self.backGroundSetUp()
 
     def backGroundSetUp(self):
         #remember to set as a memeber var with self.var otherwise is deleted after function calls
-        self.backGroundPic = tk.PhotoImage(file = "bidenTDM.png")
-        backGroundLabel = tk.Label(self,image=self.backGroundPic)
-        backGroundLabel.pack()
+        self.backGroundLabel = tk.Label(self,image=self.backGroundPic)
+        self.backGroundLabel.place(x=0,y=0)
+        self.selectSceneButton = tk.Button(self,text="scene select",command= lambda: self.showSceneSelect())
+        self.selectSceneButton.place(x=0,y=0)
+
+    def showSceneSelect(self):
+        self.backGroundLabel.destroy()
+        self.backGroundPic = tk.PhotoImage(file = "images\\sceneSelectBackGround.png")
+        self.backGroundLabel = tk.Label(self,image=self.backGroundPic)
+        self.backGroundLabel.place(x=0,y=0)
+        sceneCount = 0
+        
+        for filename in listdir('images\\scenes'):
+            # self.scenes.append(tk.PhotoImage(file = filename))
+            button = tk.Button(text=filename,width=12,height=4,command=lambda:self.changeScene(filename))
+            button.place(x=(sceneCount%6)*100 + 100,y=(int(sceneCount / 6) *100 )+ 300)
+            sceneCount += 1
+
+
+    def changeScene(self,filename):
+        self.backGroundLabel.destroy()
+        self.backGroundPic = tk.PhotoImage(file = f"images\\scenes\\{filename}")
+        self.backGroundLabel = tk.Label(self,image=self.backGroundPic)
+        self.backGroundLabel.place(x=0,y=0)
+        self.selectSceneButton.lift()
+
+        # button = tk.Button(image=self.scenes[0],width=80,height=80,text="tesst")
+        # button.place(x=sceneCount*100,y=(sceneCount % 3)*100 + 100)
+        
+    
+
         
 
